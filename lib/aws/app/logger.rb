e77d4b71b82c4ffefa9cb7b66eac796f8a517724
Ryan Alyn Porter <rap@endymion.com>
2022-10-12 23:31:39 +0000

    Log hashes.

--- lib/aws/app/logger.rb
+++ lib/aws/app/logger.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'logger'
+require 'json'
 require_relative "logger/version"
 
 module Aws
@@ -16,6 +17,22 @@ module Aws
       # For handling 'debug', 'warn', etc
       def method_missing(name, *args, &block)
         @logger.send(name, *args, &block)
+      end
+
+      def add(severity, *args, &block)
+        case args.count
+        when 1
+          @logger.add(severity, args.first, &block)
+        when 2
+          @logger.add(severity, nil,
+            args.shift +
+            "\n  " + args.shift.to_json,
+          &block)
+        end
+      end
+
+      def debug(*args, &block)
+        add(::Logger::DEBUG, *args, &block)
       end
 
     end
--- test/aws/app/logger_test.rb
+++ test/aws/app/logger_test.rb
@@ -3,6 +3,7 @@
 require 'stringio'
 require 'test_helper'
 require 'rainbow'
+require 'json'
 
 class Aws::App::LoggerTest < Test::Unit::TestCase
   puts 'Test log message: ' + @@test_message =
@@ -15,6 +16,8 @@ class Aws::App::LoggerTest < Test::Unit::TestCase
       ::Aws::App::Logger.const_defined?(:VERSION)
     end
   end
+
+  # Existing functionality from Logger, passed through.
 
   test 'output includes message' do
     $logger = ::Aws::App::Logger.new(io: output = StringIO.new)
@@ -33,8 +36,7 @@ class Aws::App::LoggerTest < Test::Unit::TestCase
     $logger.level = :info
     $logger.info @@test_message
     $logger.debug @@test_message
-    assert output.string =~ /info/i
-    assert output.string !~ /debug/i
+    assert output.string =~ /info/i and output.string !~ /debug/i
   end
 
   test 'existing formatter interface still works' do
@@ -42,6 +44,17 @@ class Aws::App::LoggerTest < Test::Unit::TestCase
     $logger.formatter = proc {|severity, time, p, msg| "TEST#{severity}: #{msg}\n" }
     $logger.debug @@test_message
     assert output.string =~ /TESTDEBUG/i
+  end
+
+  # New functionality for AWS CloudWatch
+
+  test 'logging an object with debug' do
+    $logger = ::Aws::App::Logger.new(io: output = StringIO.new)
+    $logger.debug @@test_message, {id:'10102001', total:'1295'}
+    assert(
+      output.string.include?(@@test_message) &&
+      JSON.parse(output.string.split("\n").last)['id'].eql?('10102001')
+    )
   end
 
 end
