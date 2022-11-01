9e44d87a92d8218f1aec29be03af38e6cdf2c152
Ryan Alyn Porter <rap@endymion.com>
2022-11-01 21:13:52 +0000

    Fix progname.
    It wasn't making it through to the formatter.

--- lib/logger/formatter.rb
+++ lib/logger/formatter.rb
@@ -7,6 +7,15 @@ module Aws
         Format = "[%s #%d%s] %s: %s"
         DatetimeFormat = "%Y-%m-%dT%H:%M:%S.%6N"
         def call(severity, time, progname, msg)
+          # The AWS Lambda request ID, when available, is available in an
+          # undocumented global variable that's provided by the AWS Ruby
+          # runtime: https://github.com/aws/aws-lambda-ruby-runtime-interface-client/blob/main/lib/aws_lambda_ric/lambda_log_formatter.rb#L12
+          progname = progname ||
+            begin
+              $_global_aws_request_id
+            rescue
+              nil
+            end
           Format % [
             format_datetime(time).strip,
             Process.pid,
